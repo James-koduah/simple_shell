1e1ef28406ed5b3e7b38c319cb88a45004b4a9d3
James-Koduah <jameskoduah171@gmail.com>
2022-12-20 06:57:44 -0800

    Wrote print_error function

--- shell.c
+++ shell.c
@@ -1,4 +1,10 @@
 #include "main.h"
+
+void print_error(char *str, int exit_num)
+{
+	perror(str);
+	exit(exit_num);
+}
 /**
  * main - execute a simple shell
  * @ac: argument count
@@ -19,10 +25,7 @@ int main(__attribute__((unused))int ac, char **av)
 		_getline(&line);
 		line_copy = strdup(line); /* duplicate input str*/
 		if (line_copy == NULL)
-		{
-			perror("strdup");
-			exit(1);
-		}
+			print_error("strdup", 1);
 		i = 0;
 		token = strtok(line_copy, TOKEN_DELIMITERS);
 		while (token != NULL)
@@ -32,23 +35,17 @@ int main(__attribute__((unused))int ac, char **av)
 			token = strtok(NULL, TOKEN_DELIMITERS);
 		}
 		token_args[i] = NULL;
-			full_path = search_path(token_args[0]);
+		full_path = search_path(token_args[0]);
 		exit_env(token_args[0], line, line_copy);
 		if (full_path != NULL)
 		{
 			pid = fork(); /* create fork */
 			if (pid < 0)
-			{
-				perror("fork");
-				exit(1);
-			}
+				print_error("fork", 1);
 			else if (pid == 0) /* child process */
 			{
 				if (execve(full_path, token_args, environ) < 0) /* execute commands */
-				{
-					perror(av[0]);
-					exit(1);
-				}
+					print_error(av[0], 1);
 				free(full_path);
 			}
 			else
