452d29a7f916f19991aab9e44305eeefa5a48352
James-Koduah <jameskoduah171@gmail.com>
2022-12-20 06:03:07 -0800

    betty changes

--- getline.c
+++ getline.c
@@ -2,19 +2,20 @@
 
 /**
  * _getline - take user input from command line
- *
+ * @line: A pointer from the main function that would
+ *	store the result of getline;
  * Return: void
  */
 void _getline(char **line)
 {
-	size_t n; 
+	size_t n;
 	ssize_t char_read;
 
 	char_read = getline(line, &n, stdin);
 	if (char_read == -1)
 	{
 		printf("\n");
-		free(line);
+		free(*line);
 		exit(1);
 	}
 }
